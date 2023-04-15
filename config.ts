525e4005b7c8839e89cd4d0c9eaecdf64a015444
Damet24 <damet.2401@gmail.com>
2023-04-15 15:28:47 -0500

    env vars

--- config.ts
+++ config.ts
@@ -1,8 +1,20 @@
 import dotenv from 'dotenv'
+import joi from 'joi'
 dotenv.config()
+
+const envVarsSchema = joi.object({
+  BOT_TOKEN: joi.string().required()
+})
+
+const { BOT_TOKEN } = process.env
+const { error, value: envVars } = envVarsSchema.validate({ BOT_TOKEN })
+
+if (error) {
+  throw new Error(`La validación de variables de entorno falló: ${error.message}`)
+}
 
 export default {
   bot: {
-    token: process.env.BOT_TOKEN ?? ""
+    token: envVars.BOT_TOKEN
   }
 }
