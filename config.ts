41d450ab9001639e316f6d68dc3c76149e35c50c
Damet24 <damet.2401@gmail.com>
2023-04-16 20:52:34 -0500

    env varibales updated

--- config.ts
+++ config.ts
@@ -5,11 +5,10 @@ dotenv.config()
 const envVarsSchema = joi.object({
   BOT_TOKEN: joi.string().required(),
   BOT_CLIENT_ID: joi.string().required(),
-  BOT_GUILD_ID: joi.string().required()
 })
 
-const { BOT_TOKEN, BOT_CLIENT_ID, BOT_GUILD_ID } = process.env
-const { error, value: envVars } = envVarsSchema.validate({ BOT_TOKEN, BOT_CLIENT_ID, BOT_GUILD_ID })
+const { BOT_TOKEN, BOT_CLIENT_ID } = process.env
+const { error, value: envVars } = envVarsSchema.validate({ BOT_TOKEN, BOT_CLIENT_ID })
 
 if (error) {
   throw new Error(`La validación de variables de entorno falló: ${error.message}`)
@@ -19,6 +18,5 @@ export default {
   bot: {
     token: envVars.BOT_TOKEN,
     clientId: envVars.BOT_CLIENT_ID,
-    guildId: envVars.BOT_GUILD_ID
   }
 }
