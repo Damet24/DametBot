ffe3efbfccf2aaa0f6f0fa59779a098cc962da0a
Damet24 <damet.2401@gmail.com>
2023-04-21 21:09:06 -0500

    nev contexts added

--- src/apps/botConfig/backend/dependency-injection/index.ts
+++ src/apps/botConfig/backend/dependency-injection/index.ts
@@ -1,10 +1,31 @@
-import { ContainerBuilder } from 'node-dependency-injection'
+import { ContainerBuilder, Definition, Reference } from 'node-dependency-injection'
 import WinstonLogger from '../../../../Contexts/Shared/infrastructure/WinstonLogger'
+import { SessionRepository } from '../../../../Contexts/Auth/infrastructure/SessionRepository'
+import { PostgresPoolFactory } from '../../../../Contexts/Shared/infrastructure/persistence/PoostgresPoolFactory'
+import { SessionPostController } from '../contrellers/SessionPostController'
+import { SessionCreateUseCase } from '../../../../Contexts/Auth/application/SessionCreateUseCase'
 
 const container = new ContainerBuilder()
 
-container.register('Shared.Logger', WinstonLogger)
-
-export {
-  container
+function registerFactory (_class: any, method: string, name: string): void {
+  const definition = new Definition()
+  definition.setFactory(_class, method)
+  container.setDefinition(name, definition)
 }
+
+// Shared
+container.register('Shared.Logger', WinstonLogger)
+registerFactory(PostgresPoolFactory, 'create', 'Shared.domain.PostgresPoolFactory')
+
+// Contexts
+container.register('Auth.Session.domain.SessionRepository', SessionRepository)
+  .addArgument(new Reference('Shared.domain.PostgresPoolFactory'))
+
+container.register('Auth.Session.application.SessionCreateUseCase', SessionCreateUseCase)
+  .addArgument(new Reference('Auth.Session.domain.SessionRepository'))
+
+// Apps
+container.register('Apps.backend.contollers.SessionPostController', SessionPostController)
+  .addArgument(new Reference('Auth.Session.application.SessionCreateUseCase'))
+
+export default container
