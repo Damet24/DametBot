8d188a54f75ae9f0500df30e000c30e045ca25fa
Damet24 <damet.2401@gmail.com>
2023-04-22 16:07:24 -0500

    Dependency injections refactor add modules

--- src/apps/botConfig/backend/dependency-injection/index.ts
+++ src/apps/botConfig/backend/dependency-injection/index.ts
@@ -1,33 +1,23 @@
-import { ContainerBuilder, Definition, Reference } from 'node-dependency-injection'
-import WinstonLogger from '../../../../Contexts/Shared/infrastructure/WinstonLogger'
-import { SessionRepository } from '../../../../Contexts/Auth/infrastructure/SessionRepository'
-import { PostgresPoolFactory } from '../../../../Contexts/Shared/infrastructure/persistence/PoostgresPoolFactory'
-import { SessionPostController } from '../contrellers/SessionPostController'
-import { SessionCreateUseCase } from '../../../../Contexts/Auth/application/SessionCreateUseCase'
-import { MysqlPoolFactory } from '../../../../Contexts/Shared/infrastructure/persistence/MysqlPoolFactory'
+import { ContainerBuilder, Definition } from 'node-dependency-injection'
+import { loadSharedDependencies } from './Shared'
+import { loadAuthDependencies } from './Auth'
+import { loadAppsDependencies } from './apps'
 
 const container = new ContainerBuilder()
 
-function registerFactory (_class: any, method: string, name: string): void {
+export function registerFactory (_class: any, method: string, name: string): void {
   const definition = new Definition()
   definition.setFactory(_class, method)
   container.setDefinition(name, definition)
 }
 
 // Shared
-container.register('Shared.Logger', WinstonLogger)
-registerFactory(PostgresPoolFactory, 'create', 'Shared.domain.PostgresPoolFactory')
-registerFactory(MysqlPoolFactory, 'create', 'Shared.domain.MysqlPoolFactory')
+loadSharedDependencies(container)
 
 // Contexts
-container.register('Auth.Session.domain.SessionRepository', SessionRepository)
-  .addArgument(new Reference('Shared.domain.MysqlPoolFactory'))
-
-container.register('Auth.Session.application.SessionCreateUseCase', SessionCreateUseCase)
-  .addArgument(new Reference('Auth.Session.domain.SessionRepository'))
+loadAuthDependencies(container)
 
 // Apps
-container.register('Apps.backend.contollers.SessionPostController', SessionPostController)
-  .addArgument(new Reference('Auth.Session.application.SessionCreateUseCase'))
+loadAppsDependencies(container)
 
 export default container
