0573302d2b7f96e783de5c65979e648f8b9cbc96
Damet24 <damet.2401@gmail.com>
2023-04-22 17:32:51 -0500

    dependency injection updated

--- src/apps/botConfig/backend/dependency-injection/Auth/index.ts
+++ src/apps/botConfig/backend/dependency-injection/Auth/index.ts
@@ -1,11 +1,21 @@
 import { Reference, type ContainerBuilder } from 'node-dependency-injection'
 import { SessionRepository } from '../../../../../Contexts/Auth/infrastructure/SessionRepository'
 import { SessionCreateUseCase } from '../../../../../Contexts/Auth/application/SessionCreateUseCase'
+import { UserRepository } from '../../../../../Contexts/Auth/infrastructure/UserRepository'
+import { UserCreateUseCase } from '../../../../../Contexts/Auth/application/UserCreateUseCase'
 
 export function loadAuthDependencies (container: ContainerBuilder): void {
+  // Session
   container.register('Auth.Session.domain.SessionRepository', SessionRepository)
     .addArgument(new Reference('Shared.domain.MysqlPoolFactory'))
 
   container.register('Auth.Session.application.SessionCreateUseCase', SessionCreateUseCase)
     .addArgument(new Reference('Auth.Session.domain.SessionRepository'))
+
+  // User
+  container.register('Auth.User.domain.UserRepository', UserRepository)
+    .addArgument(new Reference('Shared.domain.MysqlPoolFactory'))
+
+  container.register('Auth.User.application.UserCreateUseCase', UserCreateUseCase)
+    .addArgument(new Reference('Auth.User.domain.UserRepository'))
 }
--- src/apps/botConfig/backend/dependency-injection/apps/index.ts
+++ src/apps/botConfig/backend/dependency-injection/apps/index.ts
@@ -1,7 +1,13 @@
 import { Reference, type ContainerBuilder } from 'node-dependency-injection'
 import { SessionPostController } from '../../contrellers/SessionPostController'
+import { UserPostController } from '../../contrellers/UserPostController'
 
 export function loadAppsDependencies (container: ContainerBuilder): void {
-  container.register('Apps.backend.contollers.SessionPostController', SessionPostController)
+  // Session
+  container.register('Apps.backend.controllers.SessionPostController', SessionPostController)
     .addArgument(new Reference('Auth.Session.application.SessionCreateUseCase'))
+
+  // User
+  container.register('Apps.backend.controllers.UserPostController', UserPostController)
+    .addArgument(new Reference('Auth.User.application.UserCreateUseCase'))
 }
