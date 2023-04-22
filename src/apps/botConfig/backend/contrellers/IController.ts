5f9173bb5bcb680f3ba721a732c5572068f63462
Damet24 <damet.2401@gmail.com>
2023-04-22 17:39:01 -0500

    controller interface updated

--- src/apps/botConfig/backend/contrellers/IController.ts
+++ src/apps/botConfig/backend/contrellers/IController.ts
@@ -1,5 +1,5 @@
-import { type Request, type Response } from 'express'
+import { type NextFunction, type Request, type Response } from 'express'
 
 export interface IController {
-  run: (req: Request<any>, res: Response) => Promise<void>
+  run: (req: Request<any>, res: Response, next: NextFunction) => Promise<void>
 }
