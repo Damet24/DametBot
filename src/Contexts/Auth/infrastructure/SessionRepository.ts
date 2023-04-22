7deb6670a026e49356e251ce42cb911cc9ba2544
Damet24 <damet.2401@gmail.com>
2023-04-21 20:58:52 -0500

    Auth inf Session repository

---
+++ src/Contexts/Auth/infrastructure/SessionRepository.ts
@@ -0,0 +1,36 @@
+import { type ISessionRepository } from '../domain/ISessionRepository'
+import { Session } from '../domain/Session'
+import { type Pool } from 'mysql'
+
+export class SessionRepository implements ISessionRepository {
+  constructor (private readonly pool: Pool) { }
+
+  async saveSession (userId: string, token: string): Promise<void> {
+    await new Promise<void>((resolve, reject) => {
+      const session = new Session(null, userId, token)
+
+      this.pool.query('INSERT INTO sessions SET ?', session, (error, _data) => {
+        if (error != null) reject(error)
+        resolve()
+      })
+    })
+  }
+
+  async getSessionByToken (token: string): Promise<Session | undefined> {
+    return await new Promise<Session | undefined>((resolve, reject) => {
+      this.pool.query(`SELECT * FROM sessions WHERE token = ${token}`, (error, data) => {
+        if (error != null) reject(error)
+        resolve(data)
+      })
+    })
+  }
+
+  async deleteSession (token: string): Promise<void> {
+    await new Promise<void>((resolve, reject) => {
+      this.pool.query(`DELETE FROM sessions WHERE token = ${token}`, (error, _data) => {
+        if (error != null) reject(error)
+        resolve()
+      })
+    })
+  }
+}
