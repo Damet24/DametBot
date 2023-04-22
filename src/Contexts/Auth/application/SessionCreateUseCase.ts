2b99a8986fa8ac79a0b706493431effaf56e8ac4
Damet24 <damet.2401@gmail.com>
2023-04-21 22:03:21 -0500

    format files

--- src/Contexts/Auth/application/SessionCreateUseCase.ts
+++ src/Contexts/Auth/application/SessionCreateUseCase.ts
@@ -1,10 +1,10 @@
-import { IUseCase } from "../../Shared/domain/IUseCase";
-import { ISessionRepository } from "../domain/ISessionRepository";
+import { type IUseCase } from '../../Shared/domain/IUseCase'
+import { type ISessionRepository } from '../domain/ISessionRepository'
 
 export class SessionCreateUseCase implements IUseCase<{ userId: string, token: string }, void> {
-  constructor(private repository: ISessionRepository) { }
+  constructor (private readonly repository: ISessionRepository) { }
 
-  async run(params: { userId: string, token: string }): Promise<void> {
+  async run (params: { userId: string, token: string }): Promise<void> {
     await this.repository.saveSession(params.userId, params.token)
   }
 }
