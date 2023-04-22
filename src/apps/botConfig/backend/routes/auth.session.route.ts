47109dd3e696f544af40ab3bac04233ecba41c9d
Damet24 <damet.2401@gmail.com>
2023-04-22 17:31:49 -0500

    session  routes  updated

---
+++ src/apps/botConfig/backend/routes/auth.session.route.ts
@@ -0,0 +1,12 @@
+import { type Express } from 'express'
+import container from '../dependency-injection'
+import { type SessionPostController } from '../contrellers/SessionPostController'
+
+export const register = async (app: Express): Promise<void> => {
+  const sessionPostController: SessionPostController = container.get(
+    'Apps.backend.controllers.SessionPostController'
+  )
+
+  // eslint-disable-next-line
+  app.post('/session', sessionPostController.run.bind(sessionPostController))
+}
