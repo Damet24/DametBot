6d5a116f3e6f0c957182c6e7b2481a2ddae058a0
Damet24 <damet.2401@gmail.com>
2023-04-22 17:33:01 -0500

    user repository updated

--- src/Contexts/Auth/domain/IUserRepository.ts
+++ src/Contexts/Auth/domain/IUserRepository.ts
@@ -1,5 +1,6 @@
 import { type User } from './User'
 
 export interface IUserRepasitory {
+  saveUser: (userData: { username: string }) => Promise<void>
   getUserByUsername: (username: string) => Promise<User | undefined>
 }
